d319530891bdf4ac5abc0e43aedaa0ce68356464
AmirhosseinRHT <amirhosseinrahati@gmail.com>
2024-10-15 13:20:18 +0330

    Added maximum cpu freq to processorInfo

--- CA0 - toturial/processorInfo.cpp
+++ CA0 - toturial/processorInfo.cpp
@@ -4,9 +4,38 @@
 #include <bitset>
 #include <vector>
 
+void getCPUFrequencyInfo(int& baseFrequency, int& maxFrequency) {
+    int cpuInfo[4] = {-1};
+
+    __cpuid(cpuInfo, 0x16);
+    baseFrequency = cpuInfo[0];
+
+    maxFrequency = 0;
+    __cpuid(cpuInfo, 0x80000007);
+    std::bitset<32> edxBits(cpuInfo[3]);
+    if (edxBits[8]) { 
+        __cpuid(cpuInfo, 0x80000004);
+        unsigned int eax = cpuInfo[0];
+
+        std::string freqString;
+        for (int i = 0; i < 16; ++i) {
+            if (((char*)&eax)[i] >= '0' && ((char*)&eax)[i] <= '9') {
+                while (i < 16 && ((char*)&eax)[i] != ' ') {
+                    freqString += ((char*)&eax)[i++];
+                }
+                break;
+            }
+        }
+
+        if (!freqString.empty()) {
+            maxFrequency = std::stof(freqString) * 1000;
+        }
+    }
+}
+
 void get_processor_info() {
     int cpu_info[4] = {-1};
-    char processor_name[49];
+    char processor_name[49] = {0};
     
     __cpuid(cpu_info, 0x80000002);
     memcpy(processor_name, cpu_info, sizeof(cpu_info));
@@ -37,6 +66,15 @@ void get_processor_info() {
     }
     std::cout << std::endl;
 
+    // Add frequency information
+    int baseFrequency, maxFrequency;
+    getCPUFrequencyInfo(baseFrequency, maxFrequency);
+    std::cout << "Base Frequency: " << baseFrequency << " MHz" << std::endl;
+    if (maxFrequency > 0) {
+        std::cout << "Max Frequency: " << maxFrequency << " MHz" << std::endl;
+    } else {
+        std::cout << "Max Frequency: Not available" << std::endl;
+    }
 }
 
 int main() {
