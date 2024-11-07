05e54f953e2aaecef80492fed91a73a291455daf
AmirhosseinRHT <amirhosseinrahati@gmail.com>
2024-11-07 16:41:07 +0330

    Added timer for serial part

--- CA1 - SIMD/codes/Q3/main.cpp
+++ CA1 - SIMD/codes/Q3/main.cpp
@@ -3,11 +3,20 @@
 #include <sstream>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
 
 using namespace std;
 
 const int ARRAY_SIZE = 50;
 const unsigned int SEED = 10;
+
+uint64_t micros()
+{
+    uint64_t us = chrono::duration_cast<chrono::microseconds>(
+            chrono::high_resolution_clock::now().time_since_epoch())
+            .count();
+    return us; 
+}
 
 void generateRandomChars(char *arr, int size) {
     const char charset[] = "ABCDEF";
@@ -19,7 +28,9 @@ void generateRandomChars(char *arr, int size) {
     arr[size] = '\0';
 }
 
-string runLengthEncodingSerial(const char *input) {
+long runLengthEncodingSerial(const char *input) {
+    long long start, end;
+    start = micros();
     string output;
     while (*input != '\0') {
         char currentChar = *input;
@@ -32,7 +43,9 @@ string runLengthEncodingSerial(const char *input) {
         output += to_string(count);
         input++;
     }
-    return output;
+    cout << "Run Length Encoding: " << output << endl;
+    end = micros();
+    return end - start;
 }
 
 int main() {
@@ -40,8 +53,8 @@ int main() {
     char *arr = new char[ARRAY_SIZE + 1];
     generateRandomChars(arr, ARRAY_SIZE);
     cout << "Input: " << arr << endl;
-    string res = runLengthEncodingSerial(arr);
-    cout << "Run Length Encoding: " << res << endl;
+    long serialTime = runLengthEncodingSerial(arr);
+    cout << "Serial Time: " << serialTime << endl;
     delete[] arr;
     return 0;
 }
