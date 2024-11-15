1433fec282ff3938b762f914551e4d740996e15e
AmirhosseinRHT <amirhosseinrahati@gmail.com>
2024-11-15 23:27:08 +0330

    Updated parallel part of Q3

--- CA2 - OpenMP/Q3/main.cpp
+++ CA2 - OpenMP/Q3/main.cpp
@@ -1,45 +1,24 @@
 
-    #include <iostream>
-    #include <chrono>
-    #include <omp.h>
+#include <iostream>
+#include <chrono>
+#include <omp.h>
 
-    const long long SIDE = 1000;
-    using namespace std;
+const long long SIDE = 20000;
+using namespace std;
 
 
-    uint64_t micros()
-    {
-        uint64_t us = chrono::duration_cast<chrono::microseconds>(
-                chrono::high_resolution_clock::now().time_since_epoch())
-                .count();
-        return us; 
-    }
+uint64_t micros(){
+    uint64_t us = chrono::duration_cast<chrono::microseconds>(
+            chrono::high_resolution_clock::now().time_since_epoch())
+            .count();
+    return us; 
+}
 
-    long calcPiSerial() {
-        float x, y, origin_dist;
-        int circle_points = 0;
-        long long start , end;
-        start = micros();
-        for (int i = 0; i < (SIDE * SIDE); i++) {
-            x = float(rand() % (SIDE)) / SIDE;
-            y = float(rand() % (SIDE)) / SIDE;
-            origin_dist = x * x + y * y;
-            if (origin_dist < 1)
-                circle_points++;
-        }
-        float pi = float(4 * circle_points) / (SIDE * SIDE);
-        end = micros();
-        cout << "Serial Result : " << pi << endl;
-        return end - start;
-    }
-
-    long calcPiParallel() {
+long long calcPiSerial() {
     float x, y, origin_dist;
     int circle_points = 0;
-    long long start, end;
+    long long start , end;
     start = micros();
-
-    #pragma omp parallel loop private(circle_points) reduction(+:circle_points)
     for (int i = 0; i < (SIDE * SIDE); i++) {
         x = float(rand() % (SIDE)) / SIDE;
         y = float(rand() % (SIDE)) / SIDE;
@@ -49,18 +28,39 @@
     }
     float pi = float(4 * circle_points) / (SIDE * SIDE);
     end = micros();
+    cout << "Serial Result : " << pi << endl;
+    return end - start;
+}
+
+long long calcPiParallel() {
+    double x, y, origin_dist;
+    int circle_points = 0;
+    long long start, end;
+
+    start = micros();
+    #pragma omp parallel for private(x, y, origin_dist) reduction(+:circle_points)
+    for (int i = 0; i < (SIDE * SIDE); i++) {
+        x = double(rand() % (SIDE)) / SIDE;
+        y = double(rand() % (SIDE)) / SIDE;
+        origin_dist = x * x + y * y;
+        if (origin_dist < 1)
+            circle_points++;
+    }
+
+    double pi = double(4 * circle_points) / (SIDE * SIDE);
+    end = micros();
+
     cout << "Parallel Result : " << pi << endl;
     return end - start;
 }
 
-    int main()
-    {
-        srand(time(NULL));
-        long serialTime = calcPiSerial();
-        long parallelTime = calcPiParallel();
-        cout << "Serial Time : " << serialTime << " microseconds" << endl;
-        cout << "Parallel Time : " << parallelTime << " microseconds" << endl;
-        float speedUp = float(serialTime  / parallelTime);
-        cout << "SpeedUp: " << speedUp << endl;
-        return 0;
-    }
+int main(){
+    srand(time(NULL));
+    long long serialTime = calcPiSerial();
+    long long parallelTime = calcPiParallel();
+    cout << "Serial Time : " << serialTime << " microseconds" << endl;
+    cout << "Parallel Time : " << parallelTime << " microseconds" << endl;
+    double speedUp = double (serialTime) / double (parallelTime);
+    cout << "Speed Up: " << speedUp << endl;
+    return 0;
+}
