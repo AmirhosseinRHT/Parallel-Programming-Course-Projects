ae16db858fe4d3872735b927b45edbd6665e1334
Farbod82 <farbodazimmohseni@gmail.com>
2024-11-19 08:04:00 +0330

    the first two questions need to be completed

--- CA2 - OpenMP/Q2/main.cpp
+++ CA2 - OpenMP/Q2/main.cpp
@@ -15,20 +15,22 @@ uint64_t micros()
 }
 
 
-long long serial(){
+long long serial(double c_i, double c_j){
     long long start, end;
     cv::Mat image(800, 800, CV_8UC3, cv::Scalar(0, 0, 0));
 	start = micros();
+
+    double z_i,z_c;
      for (int i = 0; i < 800; i++) {
         for (int j = 0; j < 800; j++) {
             double x = -2.0 + (i * (4.0)) / 800.0;
             double y = -2.0 + (j * (4.0)) / 800.0;
             bool converge = true;
-            double z_i = 0;
-            double z_c = 0;
+            z_i = x;
+            z_c = y;
             for(int itr =0; itr < 1000; itr++){
-                double temp_i = pow(z_i,2) - pow(z_c,2) + x;
-                double temp_c = (2 * z_i * z_c) + y;
+                double temp_i = pow(z_i,2) - pow(z_c,2) + c_i;
+                double temp_c = (2 * z_i * z_c) + c_j;
                 z_i = temp_i;
                 z_c = temp_c;
                 if ((z_i * z_i + z_c * z_c) > 4){
@@ -45,12 +47,12 @@ long long serial(){
 	end = micros();
     // cv::imshow("Mandelbrot Set", image);
     // cv::waitKey(0);
-    cv::imwrite("C:/Users/farbo/Desktop/university-s7/PP/Parallel-Programming/CA2 - OpenMP/Q1/ser.png", image);
+    cv::imwrite("C:/Users/farbo/Desktop/university-s7/PP/Parallel-Programming/CA2 - OpenMP/Q2/ser.png", image);
     return end - start;
 }
 
 
-long long parallel(){
+long long parallel(double c_i,double c_j){
     long long start, end;
     cv::Mat image(800, 800, CV_8UC3, cv::Scalar(0, 0, 0));
 	start = micros();
@@ -58,17 +60,17 @@ long long parallel(){
     double z_i,z_c;
     bool converge;
     double temp_i,temp_c;
-    #pragma omp parallel for collapse(2) shared(image) private(z_i,z_c,x,y,converge,temp_i,temp_c)
+    #pragma omp parallel for collapse(2) shared(image,c_i,c_j) private(z_i,z_c,x,y,converge,temp_i,temp_c)
     for (int i = 0; i < 800; i++) {
         for (int j = 0; j < 800; j++) {
             x = -2.0 + (i * (4.0)) / 800.0;
             y = -2.0 + (j * (4.0)) / 800.0;
             converge = true;
-            z_i = 0;
-            z_c = 0;
-            for(int itr =0; itr < 1000; itr++){
-                temp_i = pow(z_i,2) - pow(z_c,2) + x;
-                temp_c = (2 * z_i * z_c) + y;
+            z_i = x;
+            z_c = y;
+            for(int itr =0; itr < 50; itr++){
+                temp_i = pow(z_i,2) - pow(z_c,2) + c_i;
+                temp_c = (2 * z_i * z_c) + c_j;
                 z_i = temp_i;
                 z_c = temp_c;
                 if ((z_i * z_i + z_c * z_c) > 4){
@@ -85,14 +87,17 @@ long long parallel(){
 	end = micros();
     // cv::imshow("Mandelbrot Set", image);
     // cv::waitKey(0);
-    cv::imwrite("C:/Users/farbo/Desktop/university-s7/PP/Parallel-Programming/CA2 - OpenMP/Q1/par.png", image);
+    cv::imwrite("C:/Users/farbo/Desktop/university-s7/PP/Parallel-Programming/CA2 - OpenMP/Q2/par.png", image);
     return end - start;
 }
 
 
 int main(){
-    long long ser_time = serial();
-	long long par_time = parallel();
+    long long ser_time = serial(0.285,0.01);
+	long long par_time = parallel(0.285,0.01);
+    // long long ser_time = serial(0.0,0.0);
+	// long long par_time = parallel(0.0,0.0);
+
 	std::cout << "Serial Time: " <<ser_time <<std::endl;
 	std::cout << "parallel Time: " <<par_time <<std::endl;
 	std::cout << "Speed Up: " << (double) ser_time / (double) par_time << std::endl;
