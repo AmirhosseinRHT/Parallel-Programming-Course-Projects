62b67f8a63f033a513e11cc57b6122a62b56db43
Farbod82 <farbodazimmohseni@gmail.com>
2024-11-19 22:40:35 +0330

    added ratio and parametrized Q1

--- CA2 - OpenMP/Q1/main.cpp
+++ CA2 - OpenMP/Q1/main.cpp
@@ -15,19 +15,19 @@ uint64_t micros()
 }
 
 
-long long serial(){
+long long serial(int height,int width,float ratio){
     long long start, end;
-    cv::Mat image(800, 800, CV_8UC3, cv::Scalar(0, 0, 0));
+    cv::Mat image(height, width, CV_8UC3, cv::Scalar(0, 0, 0));
 	start = micros();
-     for (int i = 0; i < 800; i++) {
-        for (int j = 0; j < 800; j++) {
-            double x = -2.0 + (i * (4.0)) / 800.0;
-            double y = -2.0 + (j * (4.0)) / 800.0;
+     for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            double x = -ratio + (i * (2*ratio)) / height;
+            double y = -ratio + (j * (2*ratio)) / width;
             bool converge = true;
             double z_i = 0;
             double z_c = 0;
             for(int itr =0; itr < 1000; itr++){
-                double temp_i = pow(z_i,2) - pow(z_c,2) + x;
+                double temp_i = (z_i*z_i) - (z_c*z_c) + x;
                 double temp_c = (2 * z_i * z_c) + y;
                 z_i = temp_i;
                 z_c = temp_c;
@@ -50,24 +50,24 @@ long long serial(){
 }
 
 
-long long parallel(){
+long long parallel(int height,int width,float ratio){
     long long start, end;
-    cv::Mat image(800, 800, CV_8UC3, cv::Scalar(0, 0, 0));
+    cv::Mat image(height, width, CV_8UC3, cv::Scalar(0, 0, 0));
 	start = micros();
     double x,y;
     double z_i,z_c;
     bool converge;
     double temp_i,temp_c;
-    #pragma omp parallel for collapse(2) shared(image) private(z_i,z_c,x,y,converge,temp_i,temp_c)
-    for (int i = 0; i < 800; i++) {
-        for (int j = 0; j < 800; j++) {
-            x = -2.0 + (i * (4.0)) / 800.0;
-            y = -2.0 + (j * (4.0)) / 800.0;
+    #pragma omp parallel for shared(image) private(z_i,z_c,x,y,converge,temp_i,temp_c) schedule(static, 10)
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            x = -ratio + (i * (2*ratio)) / height;
+            y = -ratio + (j * (2*ratio)) / width;
             converge = true;
             z_i = 0;
             z_c = 0;
             for(int itr =0; itr < 1000; itr++){
-                temp_i = pow(z_i,2) - pow(z_c,2) + x;
+                temp_i = (z_i*z_i) - (z_c*z_c) + x;
                 temp_c = (2 * z_i * z_c) + y;
                 z_i = temp_i;
                 z_c = temp_c;
@@ -91,8 +91,10 @@ long long parallel(){
 
 
 int main(){
-    long long ser_time = serial();
-	long long par_time = parallel();
+    int height = 800;
+    int width = 800;
+    long long ser_time = serial(height,width,2);
+	long long par_time = parallel(height,width,2);
 	std::cout << "Serial Time: " <<ser_time <<std::endl;
 	std::cout << "parallel Time: " <<par_time <<std::endl;
 	std::cout << "Speed Up: " << (double) ser_time / (double) par_time << std::endl;
